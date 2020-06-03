0f2a3820116c0da035fb8bf49d8b736baaf382db
bogdan-todorovic <bogdantodorovic93@gmail.com>
2020-06-03 11:17:29 +0200

    integrated frontend production build

---
+++ build/precache-manifest.1595e18343e38fadce2dcd394c06b239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faca083f19d51ac11068c07ecd93c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afc454fc9487b7101e37",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "05945b357eb1f5da6df3",
+    "url": "/static/js/2.40776c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40776c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc454fc9487b7101e37",
+    "url": "/static/js/main.a3747d5e.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
---
+++ build/precache-manifest.87f68b91b84ec928023e7a2359649429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa3a47cd01dc94ba40089835f735d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0e74a0ca0d127440cc",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "05945b357eb1f5da6df3",
+    "url": "/static/js/2.40776c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40776c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0e74a0ca0d127440cc",
+    "url": "/static/js/main.c525e5e0.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
